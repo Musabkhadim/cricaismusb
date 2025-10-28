ca5cf6cba73c886c81da74662f401020059bf88b
Musab Khadim <musabkhadim363@gmail.com>
2025-10-28 15:05:44 +0500

    don

--- components/add-business-modal.tsx
+++ components/add-business-modal.tsx
@@ -555,7 +555,7 @@ export function AddBusinessModal({ open, onOpenChange }: AddBusinessModalProps) 
                     </div>
                   </div>
                   <div>
-                    <Label htmlFor="website">Website (Optional)</Label>
+                    <Label htmlFor="website">Website </Label>
                     <Input
                       id="website"
                       value={formData.website}
